249bc5a3aa1bd7d633007f067d66b14c29aa19cf
DomRezmer <dom.rezmer@gmail.com>
2023-09-08 13:24:13 +0200

    Springboot init

---
+++ src/main/java/dev/kbe/warenkorbbackend/WarenkorbBackendApplication.java
@@ -0,0 +1,13 @@
+package dev.kbe.warenkorbbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarenkorbBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WarenkorbBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/kbe/warenkorbbackend/WarenkorbBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.kbe.warenkorbbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarenkorbBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
