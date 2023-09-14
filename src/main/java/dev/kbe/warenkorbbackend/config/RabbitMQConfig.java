d17988dee49478f2b9469f351b3422793a2ff722
DomRezmer <dom.rezmer@gmail.com>
2023-09-15 00:47:19 +0200

    rabbitmq consumer and sender (UNTESTED!!!!)

---
+++ src/main/java/dev/kbe/warenkorbbackend/config/RabbitMQConfig.java
@@ -0,0 +1,36 @@
+package dev.kbe.warenkorbbackend.config;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.listener.MessageListenerContainer;
+import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+
+    @Value("${rabbitmq.queue}")
+    String queueName;
+
+    @Value("${rabbitmq.username}")
+    String username;
+
+    @Value("${rabbitmq.password}")
+    private String password;
+
+    @Bean
+    Queue queue() {
+        return new Queue(queueName, false);
+    }
+
+    @Bean
+    MessageListenerContainer messageListenerContainer(ConnectionFactory connectionFactory) {
+        SimpleMessageListenerContainer simpleMessageListenerContainer = new SimpleMessageListenerContainer();
+        simpleMessageListenerContainer.setConnectionFactory(connectionFactory);
+        simpleMessageListenerContainer.setQueues(queue());
+        simpleMessageListenerContainer.setMessageListener(new RabbitMQListener());
+        return simpleMessageListenerContainer;
+    }
+}
