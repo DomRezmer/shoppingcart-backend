7e52644729dd0548315631886100459170ea29aa
DomRezmer <dom.rezmer@gmail.com>
2023-09-15 00:47:33 +0200

    rabbitmq sender (untested!!!!!)

---
+++ src/main/java/dev/kbe/warenkorbbackend/config/RabbitMQSender.java
@@ -0,0 +1,25 @@
+package dev.kbe.warenkorbbackend.config;
+
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import dev.kbe.warenkorbbackend.Shoppingcart;
+
+@Service
+public class RabbitMQSender {
+    @Autowired
+    private AmqpTemplate rabbitTemplate;
+
+    @Value("${rabbitmq.exchange}")
+    private String exchange;
+
+    @Value("${rabbitmq.routingkey}")
+    private String routingkey;
+
+    public void send(Shoppingcart shoppingcart) {
+        rabbitTemplate.convertAndSend(exchange, routingkey, shoppingcart);
+        System.out.println("Send msg = " + shoppingcart);
+    }
+}
