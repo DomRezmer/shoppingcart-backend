b8d0fb18d3a90976e564078db4b36fb6d1923143
DomRezmer <dom.rezmer@gmail.com>
2023-09-15 15:45:09 +0200

    more rabbit stuff

--- src/main/java/dev/kbe/warenkorbbackend/config/RabbitMQConfig.java
+++ src/main/java/dev/kbe/warenkorbbackend/config/RabbitMQConfig.java
@@ -1,14 +1,22 @@
 package dev.kbe.warenkorbbackend.config;
 
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.core.Queue;
 
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.rabbit.listener.MessageListenerContainer;
 import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class RabbitMQConfig {
@@ -16,22 +24,61 @@ public class RabbitMQConfig {
 
     String queueName;
 
+    public String getQueue() {
+        return queueName;
+    }
+
     @Value("${rabbitmq.username}")
     String username;
 
     @Value("${rabbitmq.password}")
     private String password;
 
+    @Value("${rabbitmq.exchange}")
+    String exchange;
+
+    @Value("${rabbitmq.routingkey}")
+    String routingkey;
+
     @Bean
-    Queue queue() {
+    Queue shoppingcartQueue() {
         return new Queue(queueName, false);
+    }
+
+    @Bean
+    DirectExchange productExchange() {
+        return new DirectExchange(exchange);
+    }
+
+    @Bean
+    Binding binding(Queue productQueue, DirectExchange productExchange) {
+        return BindingBuilder.bind(productQueue).to(productExchange).with(routingkey);
+    }
+
+    @Bean
+    public MessageConverter jsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    @Primary
+    public AmqpTemplate customRabbitTemplate() {
+        CachingConnectionFactory connectionFactory = new CachingConnectionFactory();
+        connectionFactory.setHost("localhost");
+        connectionFactory.setPort(5672);
+        connectionFactory.setUsername(username);
+        connectionFactory.setPassword(password);
+
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(jsonMessageConverter());
+        return rabbitTemplate;
     }
 
     @Bean
     MessageListenerContainer messageListenerContainer(ConnectionFactory connectionFactory) {
         SimpleMessageListenerContainer simpleMessageListenerContainer = new SimpleMessageListenerContainer();
         simpleMessageListenerContainer.setConnectionFactory(connectionFactory);
-        simpleMessageListenerContainer.setQueues(queue());
+        simpleMessageListenerContainer.setQueues(shoppingcartQueue());
         simpleMessageListenerContainer.setMessageListener(new RabbitMQListener());
         return simpleMessageListenerContainer;
     }
